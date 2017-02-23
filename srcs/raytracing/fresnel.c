0f7d0f02bf3c660c164f8917ae34ab170b1b7d0e
Alrick EMILIEN <aemilien@e1r4p13.42.fr>
2017-02-23 18:46:09 +0100

    [update] shlick's approximation a cote de fresnel formula

--- srcs/raytracing/fresnel.c
+++ srcs/raytracing/fresnel.c
@@ -11,7 +11,33 @@ void	ft_dswap(double *a, double *b)
 
 double	fresnel(t_ray ray, t_surface s)
 {
-	double	cos_in;
+
+	double	r0;
+	double	etai;
+	double	etat;
+	double	n;
+	double	sin_t;
+	double	cos_x;
+	double	x;
+	double ret;
+
+	etai = 1;
+	etat = s.refraction;
+	r0 = (etai - etat) / (etai + etat);
+	r0 *= r0;
+	cos_x = -dot_product(s.n, ray.dir);
+	if (etai > etat)
+	{
+		n = etai / etat;
+		sin_t = n * n * (1 - cos_x);
+		if (sin_t >= 1)
+			return (1);
+		cos_x = sqrt(1 - sin_t);
+	}
+	x = 1 - cos_x;
+	ret = r0 + (1 - r0)*x*x*x*x*x;
+	return (0.1 + (1 - 0.1) * ret);
+	/*double	cos_in;
 	double	sin_tn;
 	double	cos_tn;
 
@@ -19,9 +45,9 @@ double	fresnel(t_ray ray, t_surface s)
 	double	etat;
 
 	double	rs;
-	double	rp;
+	double	rp;*/
 
-	etai = 1;
+	/*etai = 1;
 	etat = s.refraction;
 	cos_in = dot_product(ray.dir, s.n);
 	if (cos_in > 0)
@@ -34,5 +60,5 @@ double	fresnel(t_ray ray, t_surface s)
 	rs = ((etat * cos_in) - (etai * cos_tn)) / ((etat * cos_in) + (etai *cos_tn));
 	rp = ((etai * cos_in) - (etat * cos_tn)) / ((etai * cos_in) + (etat *cos_tn));
 //	printf("rs : %lf\n", (rs * rs + rp *rp) / 2);
-	return ((rs * rs + rp *rp) / 2);
+	return ((rs * rs + rp *rp) / 2);*/
 }
