da53fb2a50fc0d7f2ba9cedec68d9dbc7056747b
Alrick EMILIEN <aemilien@e1r10p11.42.fr>
2017-04-27 10:33:44 +0200

    [norme] invert matrix normed

--- scene/la.r
+++ scene/la.r
@@ -1,10 +1,29 @@
 camera:
 {
-	from: (0 0 10)
+	from: (1 5 5)
 	to: (0 0 0)
 }
 
 
 
+
+light:
+{
+	position: (0 5 10)
+}
+
+plan:
+{
+	position: (0 0 -15)
+	normal: (0 0 1)
+}
+
+box:
+{
+	position: (0 0 1)
+	size: (1)
+	color: (0xFF960A)
+	rotation: (0 10 90)
+}
 
 
